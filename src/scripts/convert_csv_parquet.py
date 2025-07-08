c2fe852f877f470894ac5eb70301aa6dacd1be2c
Louise Delauney <louisedelauney@gmail.com>
2025-07-08 16:37:46 +0200

    update csv conversion

--- src/scripts/convert_csv_parquet.py
+++ src/scripts/convert_csv_parquet.py
@@ -6,8 +6,8 @@ import time
 from tqdm import tqdm
 import gc
 
-input_folder = "data/raw"
-output_folder = "data/bronze"
+input_folder = "/data/raw"
+output_folder = "/data/bronze"
 
 # Create output folder if it doesn't exist
 os.makedirs(output_folder, exist_ok=True)
